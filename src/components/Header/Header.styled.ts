a7e877f2e2c13159177f110129882b99989eff70
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-07 12:10:00 +0200

    chore: update Header style

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -2,9 +2,9 @@ import styled from "@emotion/styled";
 
 const Header = styled.header`
   display: flex;
-  padding-left: .6rem;
-  padding-right:.6rem;
-
+  padding-left: 0.6rem;
+  padding-right: 0.6rem;
+  color: #a1a1a1;
   & > :last-child {
     margin-left: auto;
   }
