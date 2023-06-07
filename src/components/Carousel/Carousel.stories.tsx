ef8c377774da3b0ac5ed1ec1df6084b1b8fc937e
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-07 15:20:09 +0200

    refactor: create new array and add it to Basic

--- src/components/Carousel/Carousel.stories.tsx
+++ src/components/Carousel/Carousel.stories.tsx
@@ -1,9 +1,13 @@
 import { Carousel } from "./Carousel";
+import { mocks } from "./Carousel.mock";
+import { StoryObj, Meta } from "@storybook/react";
 
-const meta = {
+const meta: Meta<Carousel> = {
   title: "components/Carousel",
 };
 
 export default meta;
 
-export const Basic = () => <Carousel />;
+const list: Carousel["data"] = new Array(3).fill(undefined).map(mocks.basic);
+
+export const Basic = () => <Carousel data={list} />;
