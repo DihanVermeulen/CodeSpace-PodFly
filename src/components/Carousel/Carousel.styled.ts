a2e0986c0b5e43b1db1697531e1c72225038c613
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-07 12:09:12 +0200

    chore: create Carousel styled components file
    Create styled components file to style Carousel component.

---
+++ src/components/Carousel/Carousel.styled.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+const Carousel = styled.section`
+  display: flex;
+  padding-left: 1rem;
+  overflow-x: scroll;
+  width: 100%;
+`;
+
+export default { Carousel };
