d2f33695ea5a204e5309a52fcf4fb9223e3a9e9b
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-07 16:56:42 +0200

    refactor: update Basic

--- src/components/Carousel/Carousel.stories.tsx
+++ src/components/Carousel/Carousel.stories.tsx
@@ -1,13 +1,21 @@
 import { Carousel } from "./Carousel";
 import { mocks } from "./Carousel.mock";
-import { StoryObj, Meta } from "@storybook/react";
+import { Meta } from "@storybook/react";
 
 const meta: Meta<Carousel> = {
   title: "components/Carousel",
+  component: Carousel,
 };
 
 export default meta;
 
 const list: Carousel["data"] = new Array(3).fill(undefined).map(mocks.basic);
 
-export const Basic = () => <Carousel data={list} />;
+export const Basic = (args: Carousel) => {
+  const { data } = args;
+  return <Carousel data={data} />;
+};
+
+Basic.args = {
+  data: list,
+};
