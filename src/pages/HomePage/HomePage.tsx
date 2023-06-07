4ff8179d963ccf2fa645af3a5ea189e89158d128
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-07 16:54:43 +0200

    chore: create base structure for home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,3 +4,26 @@ import { Carousel, Podcast } from "../../components/Carousel";
 import { useState } from "react";
 import { useEffectOnce } from "react-use";
 import { mocks } from "../../components/Carousel/Carousel.mock";
+export const HomePage = () => {
+  const [carouselPodcasts, setCarouselPodcasts] = useState<Podcast[] | null>(
+    null
+  );
+
+
+  return (
+    <>
+      <Text variant="body1" as="p" fontWeight={300}>
+        Hi, guest
+      </Text>
+      <Text variant="h5" as="h2" fontWeight={600}>
+        This week's hottest
+      </Text>
+
+      {carouselPodcasts ? (
+        <Carousel data={carouselPodcasts}></Carousel>
+      ) : (
+        <section>skeleton</section>
+      )}
+    </>
+  );
+};
