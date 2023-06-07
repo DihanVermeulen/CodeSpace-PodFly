68146933880f1f932036c572b50b0fc35c7a17b1
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-07 16:54:12 +0200

    chore: import necessary files

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
+import { Carousel, Podcast } from "../../components/Carousel";
+import { useState } from "react";
+import { useEffectOnce } from "react-use";
+import { mocks } from "../../components/Carousel/Carousel.mock";
