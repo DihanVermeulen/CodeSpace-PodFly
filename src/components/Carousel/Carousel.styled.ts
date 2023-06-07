a9cc4abf640df29b2d80a29e506745438f0f492f
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-07 12:21:04 +0200

    refactor: change to ul
    Change Carousel style to be a styled ul element.

--- src/components/Carousel/Carousel.styled.ts
+++ src/components/Carousel/Carousel.styled.ts
@@ -1,10 +1,10 @@
 import styled from "@emotion/styled";
 
-const Carousel = styled.section`
+const Carousel = styled.ul`
   display: flex;
   padding-left: 1rem;
   overflow-x: scroll;
   width: 100%;
 `;
 
-export default { Carousel };
+export default { Carousel, Card };
