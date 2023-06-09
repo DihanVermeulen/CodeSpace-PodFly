f63955885c9a0eb291ba3463a0ae2c3c47f21c66
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-09 12:12:02 +0200

    chore: create DiscoverList styled file

---
+++ src/components/DiscoverList/DiscoverList.styled.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #a1a1a1;
+`;
+
+export default { StyledLink };
