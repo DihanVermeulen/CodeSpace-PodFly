b06c9074985438c1c9bf539a88b752937595f7c4
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-09 12:36:43 +0200

    refactor: update Header style

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -1,12 +1,17 @@
 import styled from "@emotion/styled";
 
 const Header = styled.header`
+  position: fixed;
   display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding-left: 0.6rem;
   padding-right: 0.6rem;
   color: #a1a1a1;
-  & > :last-child {
-    margin-left: auto;
+  width: 100vw;
+  z-index: 999;
+  background-color: rgba(255, 255, 255);
+`;
   }
 `;
 
