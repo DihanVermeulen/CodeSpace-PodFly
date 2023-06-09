a5d519953847a79d7141723f9118ce2a5da777ee
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-09 12:21:04 +0200

    chore: create browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import { CssBaseline } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { DiscoverPage, HomePage, Root } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/discover",
+        element: <DiscoverPage />,
+      },
+    ],
+  },
+  {
+    path: "/view",
+  },
+]);
 
 const App = () => {
   return (
     <>
       <CssBaseline />
+      <RouterProvider router={router} />
     </>
   );
 };
