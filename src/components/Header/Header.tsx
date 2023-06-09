c49b29ec4618f828043b022cf4852b0e8d6b459b
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-09 12:36:07 +0200

    chore: use location to change active route style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,11 @@
 import { IconButton, CssBaseline } from "@mui/material";
 import { Menu, Search, Person } from "@mui/icons-material";
 import StyledComponents from "./Header.styled";
+import { useLocation } from "react-router-dom";
 
 export const Header = () => {
+  const location = useLocation();
+
   return (
     <>
       <CssBaseline />
@@ -11,6 +14,32 @@ export const Header = () => {
           <IconButton color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <Menu />
           </IconButton>
+        </div>
+        <div>
+          <StyledComponents.LinkBlock>
+            <li>
+              <StyledComponents.StyledLink
+                to={"/"}
+                style={{
+                  color: location.pathname === "/" ? "#AAF1CF" : "",
+                }}
+              >
+                home
+              </StyledComponents.StyledLink>
+            </li>
+            <li>
+              <StyledComponents.StyledLink to={"/discover"}   style={{
+                  color: location.pathname === "/discover" ? "#AAF1CF" : "",
+                }}>
+                discover
+              </StyledComponents.StyledLink>
+            </li>
+            <li>
+              <StyledComponents.StyledLink to={"/favourites"}>
+                favourites
+              </StyledComponents.StyledLink>
+            </li>
+          </StyledComponents.LinkBlock>
         </div>
         <div>
           <IconButton color="inherit" aria-label="search" sx={{ mr: 2 }}>
