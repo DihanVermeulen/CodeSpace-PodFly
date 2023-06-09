b21df0eb6d99ae83e8fe71caa10fcb41d2a9b806
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-09 11:06:24 +0200

    refactor: create base for DiscoverList

--- src/components/DiscoverList/DiscoverList.tsx
+++ src/components/DiscoverList/DiscoverList.tsx
@@ -1,3 +1,29 @@
-export const DiscoverList = () => {
-  return <div>DiscoverList</div>;
+import { useState } from "react";
+import { Podcast } from "../../@types/podcast";
+import { Masonry } from "@mui/lab";
+import { Preview } from "./Preview";
+import { Link } from "react-router-dom";
+
+export type DiscoverList = {
+  data: Podcast[];
+};
+
+export const DiscoverList = (props: DiscoverList) => {
+  const [data] = useState(props.data);
+
+  return (
+    <>
+      {data ? (
+        <Masonry columns={2}>
+          {data.map((item, index) => (
+            <Link to={`/view/${item.id}`}>
+              <Preview key={index} title={item.title} image={item.image} />
+            </Link>
+          ))}
+        </Masonry>
+      ) : (
+        <div>loading...</div>
+      )}
+    </>
+  );
 };
