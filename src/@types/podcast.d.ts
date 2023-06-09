f8c2c6ed4e7f0f204979dbabbf04fc60d8174a41
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-09 17:23:21 +0200

    chore: add PodcastResponse type

--- src/@types/podcast.d.ts
+++ src/@types/podcast.d.ts
@@ -7,3 +7,14 @@ export type Podcast = {
   genres: string[];
   updated?: Date;
 };
+
+export type PodcastResponse = {
+  id: string;
+  title: string;
+  image: string;
+  description: string;
+  seasons: number;
+  genres: number[];
+  updated: string;
+
+}
