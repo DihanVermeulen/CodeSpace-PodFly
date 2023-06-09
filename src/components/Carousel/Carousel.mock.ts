9b337ce26c37ca1ffa6d6622ffe0f0200f5eab40
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-09 09:44:44 +0200

    refactor: change import for podcast type

--- src/components/Carousel/Carousel.mock.ts
+++ src/components/Carousel/Carousel.mock.ts
@@ -1,9 +1,9 @@
 import { faker } from "@faker-js/faker";
-import { Podcast } from "./Carousel";
+import { Podcast } from "../../@types/podcast";
 
 export const mocks = {
   basic: (): Podcast => ({
-    id: Math.floor(Math.random() * 1000),
+    id: Math.floor(Math.random() * 1000 + 1),
     image: faker.image.urlPicsumPhotos(),
     title: faker.lorem.words({ min: 2, max: 5 }),
     genres: [faker.lorem.word()],
