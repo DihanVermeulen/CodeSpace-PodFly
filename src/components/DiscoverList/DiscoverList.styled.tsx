5591de2a49e6374dda33d62c2ffce8180ae7b840
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-09 15:14:23 +0200

    refactor: update StyledLink styles

--- src/components/DiscoverList/DiscoverList.styled.tsx
+++ src/components/DiscoverList/DiscoverList.styled.tsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 import styled from "@emotion/styled";
 
 const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
   text-decoration: none;
   color: #a1a1a1;
+  width: fit-content;
 `;
 
 export default { StyledLink };
