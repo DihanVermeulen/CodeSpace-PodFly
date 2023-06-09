70f7e59428957124e0758fd10d543fd43c9922ca
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-09 12:37:35 +0200

    refactor: update imports

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
-import { Typography } from "@mui/material";
-import { Carousel, Podcast } from "../../components/Carousel";
+import { Typography, Skeleton, Stack } from "@mui/material";
+import { Carousel } from "../../components/Carousel";
 import { useState } from "react";
 import { useEffectOnce } from "react-use";
 import { mocks } from "../../components/Carousel/Carousel.mock";
