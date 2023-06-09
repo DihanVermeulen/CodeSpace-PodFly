c509ec731686e9c8659949f0977750e8168a4ae8
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-09 17:24:04 +0200

    chore: create api creation function

--- src/api/createApi.ts
+++ src/api/createApi.ts
@@ -1,1 +1,38 @@
-export const createApi = () => {};
+import { Podcast, PodcastResponse } from "../@types/podcast";
+
+export type Api = {
+  getPodcastList: () => Promise<Error | Podcast[]>;
+};
+
+const URL = "https://podcast-api.netlify.app";
+
+const getPodcastList: Api["getPodcastList"] = () => {
+  const result = fetch(URL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Something went wrong. Try again later.");
+      }
+      return response;
+    })
+    .then((response) => response.json())
+    .then((data: PodcastResponse[]) => {
+      const result = data.map((item) => ({
+        ...item,
+        release: new Date(item.updated),
+      }));
+
+      return result;
+    })
+    .catch((err) => {
+      console.error(err);
+      return err;
+    });
+
+  return result;
+};
+
+export const createApi = (): Api => {
+  return {
+    getPodcastList,
+  };
+};
