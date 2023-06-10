59774ac8530a33055849b6f72343bffc41df47d9
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-10 12:38:40 +0200

    chore: add function to get single podcast

--- src/api/createApi.ts
+++ src/api/createApi.ts
@@ -1,10 +1,12 @@
-import { Podcast, PodcastResponse } from "../@types/podcast";
+import { IndividualPodcast, Podcast, PodcastResponse } from "../@types/podcast";
 
 export type Api = {
   getPodcastList: () => Promise<Error | Podcast[]>;
+  getIndividualPodcastList: (id: string) => Promise<Error | IndividualPodcast>;
 };
 
 const URL = "https://podcast-api.netlify.app";
+const INDIVIDUAL_PODCAST_URL = "https://podcast-api.netlify.app/id";
 
 const getPodcastList: Api["getPodcastList"] = () => {
   const result = fetch(URL)
@@ -31,8 +33,28 @@ const getPodcastList: Api["getPodcastList"] = () => {
   return result;
 };
 
+const getIndividualPodcastList: Api["getIndividualPodcastList"] = (
+  id: string
+) => {
+  const result = fetch(`${INDIVIDUAL_PODCAST_URL}/${id}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Something went wrong. Try again later.");
+      }
+      return response;
+    })
+    .then((response) => response.json())
+    .catch((err) => {
+      console.error(err);
+      return err;
+    });
+
+  return result;
+};
+
 export const createApi = (): Api => {
   return {
     getPodcastList,
+    getIndividualPodcastList,
   };
 };
