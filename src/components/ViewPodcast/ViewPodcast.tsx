69b31bcd1a459e46a8b26adf405ed1082fa56a91
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-10 12:40:28 +0200

    chore: set podcasts state to response

--- src/components/ViewPodcast/ViewPodcast.tsx
+++ src/components/ViewPodcast/ViewPodcast.tsx
@@ -3,4 +3,19 @@ export const ViewPodcast = () => {
 
   const [podcasts, setPodcasts] = useState();
 
+  useEffect(() => {
+    const handleFetchIndividualPodcast = async () => {
+      if (id)
+        try {
+          const data: IndividualPodcast | Error =
+            await api.getIndividualPodcastList(id);
+          setPodcasts(data);
+        } catch (error) {
+          console.error("Error fetching individual podcast:", error);
+        }
+    };
+
+    handleFetchIndividualPodcast();
+  }, [id]);
+
 };
