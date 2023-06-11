0469e971998c3d78d4590e7999527ac05af37000
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-11 09:07:18 +0200

    chore: create theme.ts

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#d7a6b3",
+          },
+        },
+      },
+    },
+  },
+});
