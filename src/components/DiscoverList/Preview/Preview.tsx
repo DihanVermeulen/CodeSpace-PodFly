99f7297190403dce5073433b99247a31752bdd91
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-11 14:35:16 +0200

    refactor: add season count to Preview

--- src/components/DiscoverList/Preview/Preview.tsx
+++ src/components/DiscoverList/Preview/Preview.tsx
@@ -1,20 +1,33 @@
 import StyledComponents from "./Preview.styled";
+import { useState } from "react";
+import { Skeleton } from "@mui/material";
+const { Label, Card, Image } = StyledComponents;
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export type Preview = {
   image: string;
   title: string;
+  seasons: number;
+};
+
+export type LoadingImage = {
+  src: string;
 };
 
 export const Preview = (props: Preview) => {
-  const { Label, Card, Image } = StyledComponents;
-  const { image, title } = props;
+  const { image, title, seasons } = props;
 
   return (
     <Card>
       <Image src={image} />
-      <Label variant="h1" as="h6">
-        {title}
-      </Label>
+      <dl>
+        <Label variant="h1" as="dt" fontSize={"18px"}>
+          {title}
+        </Label>
+        <Label variant="body2" as="dd" color={"#A1A1A1"} fontSize={"14px"}>
+          Seasons: {seasons}
+        </Label>
+      </dl>
     </Card>
   );
 };
