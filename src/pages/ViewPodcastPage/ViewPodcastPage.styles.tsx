c97ee8fe02c7cad6410448296417f43cc0409dd6
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-12 09:17:51 +0200

    refactor: remove Select import

--- src/pages/ViewPodcastPage/ViewPodcastPage.styles.tsx
+++ src/pages/ViewPodcastPage/ViewPodcastPage.styles.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { Grid, Select, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 const StyledImage = styled.img`
   border-radius: 20px;
