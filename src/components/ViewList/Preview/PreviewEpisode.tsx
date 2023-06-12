efc0a6bb3b74415aa1c0f344df1dea12d64de4b2
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-12 11:12:56 +0200

    refactor: add Space between title and label

--- src/components/ViewList/Preview/PreviewEpisode.tsx
+++ src/components/ViewList/Preview/PreviewEpisode.tsx
@@ -1,12 +1,15 @@
 import { IndividualPodcastEpisode } from "../../../@types/podcast";
 import StyledComponents from "./Preview.styled";
+import UtilsStyles from "../../../styles/utils.styles";
 const { EpisodeTile, EpisodeTitle, EpisodeLabel } = StyledComponents;
+const { Space } = UtilsStyles;
 
 export const PreviewEpisode = (props: IndividualPodcastEpisode) => {
   const { title, episode } = props;
   return (
     <EpisodeTile>
       <EpisodeTitle component="dt">{title}</EpisodeTitle>
+      <Space height="0.3rem"/>
       <EpisodeLabel component="dd">Episode: {episode}</EpisodeLabel>
     </EpisodeTile>
   );
