893396bb4c22092fca8adb5ecb3d4c6d550e7a8c
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-12 16:26:27 +0200

    refactor: change to use BrowserRouter instead
    Change to use BrowserRouter instead, because using createBrowserRouter puts limitations on what can be done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { DiscoverPage, FavouritesPage, HomePage, Root } from "./pages";
-import { ViewPodcast } from "./pages/ViewPodcastPage";
+import { ViewPodcastPage } from "./pages/ViewPodcastPage";
 import { theme } from "./styles";
 import { Player } from "./components/Player";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/discover",
-        element: <DiscoverPage />,
-      },
-      {
-        path: "/favourites",
-        element: <FavouritesPage />,
-      },
-    ],
-  },
-  {
-    path: "/view/:id",
-    element: <ViewPodcast />,
-  },
-]);
+import { ListenPage } from "./pages/ListenPage";
 
 const App = () => {
   return (
@@ -36,7 +12,18 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Player />
-        <RouterProvider router={router} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Root />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/favourites" element={<FavouritesPage />} />
+              <Route path="/discover" element={<DiscoverPage />} />
+            </Route>
+            <Route path="/view/:id" element={<ViewPodcastPage />} />
+            <Route path="/listen/:id" element={<ListenPage />} />
+          </Routes>
+        </BrowserRouter>
+        {/* <RouterProvider router={router} /> */}
       </ThemeProvider>
     </>
   );
