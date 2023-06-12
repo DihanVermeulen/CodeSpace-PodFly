2b65825dc0ffc7d5833bd3cbf782090ff890c0fb
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-12 17:24:25 +0200

    refactor: create AnimatedRoutes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { DiscoverPage, FavouritesPage, HomePage, Root } from "./pages";
 import { ViewPodcastPage } from "./pages/ViewPodcastPage";
 import { theme } from "./styles";
 import { Player } from "./components/Player";
 import { ListenPage } from "./pages/ListenPage";
+import { AnimatePresence } from "framer-motion";
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Root />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/favourites" element={<FavouritesPage />} />
+          <Route path="/discover" element={<DiscoverPage />} />
+        </Route>
+        <Route path="/view/:id" element={<ViewPodcastPage />} />
+        <Route path="/listen/:id" element={<ListenPage />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 const App = () => {
   return (
@@ -13,15 +32,7 @@ const App = () => {
         <CssBaseline />
         <Player />
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Root />}>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/favourites" element={<FavouritesPage />} />
-              <Route path="/discover" element={<DiscoverPage />} />
-            </Route>
-            <Route path="/view/:id" element={<ViewPodcastPage />} />
-            <Route path="/listen/:id" element={<ListenPage />} />
-          </Routes>
+          <AnimatedRoutes />
         </BrowserRouter>
         {/* <RouterProvider router={router} /> */}
       </ThemeProvider>
