1eef680a1ea587f56d5897a7faad7af514a368a5
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-12 09:28:01 +0200

    refactor: use generic to infer types
    Use generic types to say whether the ViewList component should accept the IndividualPocastSeason["episodes"] or PodcastPreview[] type.

--- src/components/ViewList/ViewList.tsx
+++ src/components/ViewList/ViewList.tsx
@@ -1,3 +1,20 @@
-export const ViewList = () => {
-  return <div>ViewList</div>;
+import { Box } from "@mui/material";
+import { Preview } from "./Preview";
+import { PodcastPreview, IndividualPodcastSeason } from "../../@types/podcast";
+
+type ViewList<T = PodcastPreview[]> = {
+  data: T;
+  viewEpisodes?: boolean;
+};
+
+export const ViewList = <
+  T extends PodcastPreview[] | IndividualPodcastSeason["episodes"]
+>(
+  props: ViewList<T>
+) => {
+  const { data } = props;
+
+  console.log(data);
+
+  return <Box display={"flex"} flexWrap={"wrap"} gap={2}></Box>;
 };
