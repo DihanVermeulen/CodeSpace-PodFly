1919b59a3aa45e1aa26908de020e42514d503818
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-12 11:05:24 +0200

    refactor: add conditional render
    Add conditional render for if viewEpisodes is true.

--- src/components/ViewList/ViewList.tsx
+++ src/components/ViewList/ViewList.tsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material";
-import { Preview } from "./Preview";
+import { Box, Divider, List, ListItem } from "@mui/material";
+import { Preview, PreviewEpisode } from "./Preview";
 import { PodcastPreview, IndividualPodcastSeason } from "../../@types/podcast";
 
 type ViewList<T = PodcastPreview[]> = {
@@ -12,9 +12,36 @@ export const ViewList = <
 >(
   props: ViewList<T>
 ) => {
-  const { data } = props;
+  const { data, viewEpisodes } = props;
 
   console.log(data);
-
-  return <Box display={"flex"} flexWrap={"wrap"} gap={2}></Box>;
+  if (viewEpisodes) {
+    return (
+      <Box display={"flex"} flexWrap={"wrap"}>
+        <List sx={{ width: "100%" }}>
+          {(data as IndividualPodcastSeason["episodes"]).map((item) => (
+            <>
+              <ListItem>
+                <PreviewEpisode
+                  title={item.title}
+                  description={item.description}
+                  episode={item.episode}
+                  file={item.file}
+                />
+              </ListItem>
+              <Divider sx={{ backgroundColor: "#FFF" }} />
+            </>
+          ))}
+        </List>
+      </Box>
+    );
+  } else {
+    return (
+      <Box display={"flex"} flexWrap={"wrap"} gap={2}>
+        {data.map((item) => (
+          <Preview />
+        ))}
+      </Box>
+    );
+  }
 };
