ab4cf5510050731d9b498e54d1768ecea3bdc60b
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-13 13:19:30 +0200

    refactor: add routing logic

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,36 +1,69 @@
 import { ModalContainer } from "./Player.styled";
-import { useEffectOnce } from "react-use";
 import { IconButton } from "@mui/material";
-import { Close } from "@mui/icons-material";
+import { Close, ArrowBack } from "@mui/icons-material";
 import { createModalActions } from "../../model";
 import { getModalState } from "../../model";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export const Player = () => {
   const modalState = getModalState();
   const modalActions = createModalActions();
   const isOpen = modalState.isOpen;
+  const isMaximised = modalState.isMaximised;
+  const playerData = modalState.data;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const modalPhase = modalState.phase;
 
-  // const isLoading = useStore(store, (state) => state.isLoading);l
-  // const modalData = useStore(store, (state) => state.modalData);
-
-  const handleOpenModal = () => {
-    modalActions.openModal();
-  };
-
-  useEffectOnce(() => {
-    handleOpenModal();
-  });
+  useEffect(() => {
+    if (location.pathname.startsWith("/listen")) {
+      modalActions.maximiseModal();
+    } else {
+      modalActions.minimiseModal();
+    }
+  }, [location]);
 
   const handleCloseModal = () => {
-    modalActions.closeModal();
+    setTimeout(() => {
+      if (modalState.isMaximised) {
+        navigate(-1);
+      } else {
+        modalActions.closeModal();
+      }
+    }, 0);
+  };
+
+  const handleMaximiseModal = () => {
+    console.log("Maximising modal");
+    navigate(`/listen?showId=`, {
+      state: { background: location },
+    });
+    modalActions.maximiseModal();
   };
 
   return (
     <>
-      <ModalContainer isOpen={isOpen}>
-        <IconButton onClick={handleCloseModal}>
-          <Close />
-        </IconButton>
+      <ModalContainer
+        onClick={
+          !modalState.isMaximised
+            ? () => {
+                handleMaximiseModal();
+              }
+            : () => {}
+        }
+        isMaximised={isMaximised ? true : false}
+        isOpen={isOpen}
+      >
+        {modalState.isMaximised ? (
+          <IconButton onClick={handleCloseModal} style={{ zIndex: 1000 }}>
+            <ArrowBack />
+          </IconButton>
+        ) : (
+          <IconButton onClick={handleCloseModal} style={{ zIndex: 1000 }}>
+            <Close />
+          </IconButton>
+        )}
       </ModalContainer>
     </>
   );
