7873ba2d1b1883104600e6b9ca73f5f6f18633f8
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-13 13:20:53 +0200

    chore: add minimise and maximise modal actions

--- src/model/actions.ts
+++ src/model/actions.ts
@@ -6,6 +6,8 @@ export const createModalActions = () => {
     updateModalData: useStore(store, (state) => state.modal.data),
     openModal: useStore(store, (state) => state.modal.openModal),
     closeModal: useStore(store, (state) => state.modal.closeModal),
+    maximiseModal: useStore(store, (state) => state.modal.maximiseModal),
+    minimiseModal: useStore(store, (state) => state.modal.minimiseModal),
   };
 };
 
