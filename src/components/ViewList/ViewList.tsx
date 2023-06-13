e6fc5f0cc554d202b933af1758e2816627b7e161
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-13 16:11:35 +0200

    refactor: update types

--- src/components/ViewList/ViewList.tsx
+++ src/components/ViewList/ViewList.tsx
@@ -1,13 +1,19 @@
 import { Box, Divider, List, ListItem } from "@mui/material";
 import { Preview, PreviewEpisode } from "./Preview";
-import { PodcastPreview, IndividualPodcastSeason } from "../../@types/podcast";
+import { PodcastPreview, IndividualPodcastEpisode } from "../../@types/podcast";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { getModalState, createModalActions } from "../../model";
 import { Fragment } from "react";
 
-type ViewList<T = PodcastPreview[]> = {
+type ViewList<T> = {
   data: T;
   viewEpisodes?: boolean;
+};
+
+type EpisodeData = {
+  episodes: IndividualPodcastEpisode[];
+  podcastId: string;
+  season: number;
 };
 
 /**
@@ -15,11 +21,7 @@ type ViewList<T = PodcastPreview[]> = {
  * @param props
  * @returns episode preview or podcast preview
  */
-export const ViewList = <
-  T extends PodcastPreview[] | IndividualPodcastSeason["episodes"]
->(
-  props: ViewList<T>
-) => {
+export const ViewList = (props: ViewList<EpisodeData | PodcastPreview[]>) => {
   const { data, viewEpisodes } = props;
   if (!data) throw new Error("No data was passed");
   const modalState = getModalState();
@@ -27,45 +29,48 @@ export const ViewList = <
   const location = useLocation();
   const navigate = useNavigate();
 
-  console.log(data);
-
-  const handleNavigate = (event, episodeNumber: number) => {
+  const handleNavigate = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    episodeNumber: number
+  ) => {
     event.preventDefault();
     event.stopPropagation();
+
     if (!modalState.isOpen) modalActions.openModal();
-    navigate(`/listen?podcast=${data.podcastId}&season=${data.season}&episode=${episodeNumber}`, {
-      state: { background: location },
-    });
+
+    if ("podcastId" in data && "season" in data) {
+      navigate(
+        `/listen?podcast=${data.podcastId}&season=${data.season}&episode=${episodeNumber}`,
+        {
+          state: { background: location },
+        }
+      );
+    }
   };
 
-  console.log(data);
-
-  if (viewEpisodes) {
+  if (viewEpisodes && "episodes" in data) {
     return (
       <Box display={"flex"} flexWrap={"wrap"} style={{ width: "100%" }}>
         <List sx={{ width: "100%" }}>
-          {(data as IndividualPodcastSeason["episodes"]).episodes.map(
-            (item, index) => (
-              <Fragment key={index}>
-                <ListItem>
-                  <Link
-                    style={{ width: "100%", textDecoration: "none" }}
-                    onClick={(event) => handleNavigate(event, item.episode)}
-                    key={index}
-                    to="#"
-                  >
-                    <PreviewEpisode
-                      title={item.title}
-                      description={item.description}
-                      episode={item.episode}
-                      file={item.file}
-                    />
-                  </Link>
-                </ListItem>
-                <Divider sx={{ backgroundColor: "#FFF" }} />
-              </Fragment>
-            )
-          )}
+          {data.episodes.map((item, index) => (
+            <Fragment key={index}>
+              <ListItem>
+                <Link
+                  style={{ width: "100%", textDecoration: "none" }}
+                  onClick={(event) => handleNavigate(event, item.episode)}
+                  to="#"
+                >
+                  <PreviewEpisode
+                    title={item.title}
+                    description={item.description}
+                    episode={item.episode}
+                    file={item.file}
+                  />
+                </Link>
+              </ListItem>
+              <Divider sx={{ backgroundColor: "#FFF" }} />
+            </Fragment>
+          ))}
         </List>
       </Box>
     );
