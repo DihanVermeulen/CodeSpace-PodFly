1e618d7e5f269e7b710938f1090aa875d7f9ca2c
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-13 13:18:53 +0200

    refactor: update styles

--- src/components/Player/Player.styled.tsx
+++ src/components/Player/Player.styled.tsx
@@ -2,23 +2,27 @@ import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import { Box } from "@mui/material";
 
-export const ModalContainer = styled.div<{ isOpen: boolean }>`
+export const ModalContainer = styled.div<{
+  isOpen: boolean;
+  isMaximised: boolean;
+}>`
   display: flex;
-  align-items: center;
+  align-items: ${(props) => (props.isMaximised ? "flex-start" : "center")};
   position: fixed;
   left: 0;
   bottom: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
   width: 100%;
-  height: 10vh;
-  background-color: rgba(217, 217, 217, 0.3);
+  height: 100%;
+  background-color: ${(props) =>
+    props.isMaximised ? "rgba(255, 255, 255)" : "rgba(217, 217, 217, 0.4)"};
   transition: bottom 0.3s ease-in-out;
   z-index: 999;
-
+  height: ${(props) => (props.isMaximised ? "100%" : "10vh")};
   ${({ isOpen }) =>
     isOpen &&
     css`
-      bottom: 0; // Final position when modal is open
-    `}
+      bottom: 0;
+    `};
 `;
 
 export const Header = styled(Box)`
