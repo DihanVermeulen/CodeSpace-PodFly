2eb7aa720edfdd9368c99a84f8c775bc0264c5e0
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-13 13:20:36 +0200

    refactor: add navigation handling to Player

--- src/components/ViewList/ViewList.tsx
+++ src/components/ViewList/ViewList.tsx
@@ -1,6 +1,8 @@
 import { Box, Divider, List, ListItem } from "@mui/material";
 import { Preview, PreviewEpisode } from "./Preview";
 import { PodcastPreview, IndividualPodcastSeason } from "../../@types/podcast";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { getModalState, createModalActions } from "../../model";
 import { Fragment } from "react";
 
 type ViewList<T = PodcastPreview[]> = {
@@ -15,23 +17,55 @@ export const ViewList = <
 ) => {
   const { data, viewEpisodes } = props;
   if (!data) throw new Error("No data was passed");
+  const modalState = getModalState();
+  const modalActions = createModalActions();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  console.log(data);
+
+  const handleNavigate = (event, episodeNumber: number) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (!modalState.isOpen) modalActions.openModal();
+    navigate(`/listen?podcast=${data.podcastId}&season=${data.season}&episode=${episodeNumber}`, {
+      state: { background: location },
+    });
+  };
+
+  console.log(data);
+
   if (viewEpisodes) {
     return (
-      <Box display={"flex"} flexWrap={"wrap"}>
+      <Box
+        display={"flex"}
+        flexWrap={"wrap"}
+        style={{ width: "100%" }}
+        onClick={handleNavigate}
+      >
         <List sx={{ width: "100%" }}>
-          {(data as IndividualPodcastSeason["episodes"]).map((item, index) => (
-            <Fragment key={index}>
-              <ListItem>
-                <PreviewEpisode
-                  title={item.title}
-                  description={item.description}
-                  episode={item.episode}
-                  file={item.file}
-                />
-              </ListItem>
-              <Divider sx={{ backgroundColor: "#FFF" }} />
-            </Fragment>
-          ))}
+          {(data as IndividualPodcastSeason["episodes"]).episodes.map(
+            (item, index) => (
+              <Fragment key={index}>
+                <ListItem>
+                  <Link
+                    style={{ width: "100%", textDecoration: "none" }}
+                    onClick={(event) => handleNavigate(event, item.episode)}
+                    key={index}
+                    to="#"
+                  >
+                    <PreviewEpisode
+                      title={item.title}
+                      description={item.description}
+                      episode={item.episode}
+                      file={item.file}
+                    />
+                  </Link>
+                </ListItem>
+                <Divider sx={{ backgroundColor: "#FFF" }} />
+              </Fragment>
+            )
+          )}
         </List>
       </Box>
     );
