2a31dadb604e6e0bdcbc55875b231d53ad251faa
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-13 06:19:08 +0200

    refactor: add Link element to PreviewEpisode

--- src/components/ViewList/Preview/PreviewEpisode.tsx
+++ src/components/ViewList/Preview/PreviewEpisode.tsx
@@ -2,14 +2,17 @@ import { IndividualPodcastEpisode } from "../../../@types/podcast";
 import { EpisodeTitle, EpisodeTile, EpisodeLabel } from "./Preview.styled";
 import UtilsStyles from "../../../styles/utils.styles";
 const { Space } = UtilsStyles;
+import { Link } from "react-router-dom";
 
 export const PreviewEpisode = (props: IndividualPodcastEpisode) => {
   const { title, episode } = props;
   return (
-    <EpisodeTile>
-      <EpisodeTitle component="dt">{title}</EpisodeTitle>
-      <Space height="0.3rem" />
-      <EpisodeLabel component="dd">Episode: {episode}</EpisodeLabel>
-    </EpisodeTile>
+    <Link to={"/listen/1"} style={{ width: "100%" }}>
+      <EpisodeTile>
+        <EpisodeTitle component="dt">{title}</EpisodeTitle>
+        <Space height="0.3rem" />
+        <EpisodeLabel component="dd">Episode: {episode}</EpisodeLabel>
+      </EpisodeTile>
+    </Link>
   );
 };
