e9933ba8cfd7679a3120369cc21b01d4d3b731df
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-14 12:35:57 +0200

    refactor: move audio player func to hook

---
+++ src/hooks/useAudioPlayer.ts
@@ -0,0 +1,81 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useAudioPlayer = () => {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    const audioElement = audioRef.current;
+
+    const handleTimeUpdate = () => {
+      setCurrentTime(audioElement?.currentTime || 0);
+      setDuration(audioElement?.duration || 0);
+    };
+
+    audioElement?.addEventListener("timeupdate", handleTimeUpdate);
+
+    return () => {
+      audioElement?.removeEventListener("timeupdate", handleTimeUpdate);
+    };
+  }, [audioRef]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isDragging && audioRef.current && !audioRef.current.paused) {
+        setCurrentTime(audioRef.current.currentTime);
+      }
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isDragging, audioRef]);
+
+  const handleProgressChange = (
+    _event: React.ChangeEvent<{}>,
+    newValue: number | number[]
+  ) => {
+    if (typeof newValue === "number") {
+      setCurrentTime(newValue);
+      if (audioRef.current) audioRef.current.currentTime = newValue;
+    }
+  };
+
+  const handleDragStart = () => {
+    setIsDragging(true);
+  };
+
+  const handleDragEnd = () => {
+    setIsDragging(false);
+    if (audioRef.current) audioRef.current.currentTime = currentTime;
+  };
+
+  const handlePlayPause = () => {
+    if (audioRef.current) {
+      if (audioRef.current.paused) {
+        audioRef.current.play();
+        setIsPlaying(true);
+      } else {
+        audioRef.current.pause();
+        setIsPlaying(false);
+      }
+    }
+  };
+
+  return {
+    audioRef,
+    currentTime,
+    duration,
+    isDragging,
+    isPlaying,
+    handleProgressChange,
+    handleDragStart,
+    handleDragEnd,
+    handlePlayPause,
+  };
+};
+
+export default useAudioPlayer;
