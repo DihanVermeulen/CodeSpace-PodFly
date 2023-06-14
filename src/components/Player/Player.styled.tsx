49d8efe23bd0d1d5dfa82f6a5b459a7db29779e4
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-14 12:36:59 +0200

    refactor: update scroll container styles

--- src/components/Player/Player.styled.tsx
+++ src/components/Player/Player.styled.tsx
@@ -5,14 +5,17 @@ import { Box } from "@mui/material";
 export const ModalContainer = styled.div<{
   isOpen: boolean;
   isMaximised: boolean;
+  disableScrollLock: boolean;
 }>`
   display: flex;
+  flex-direction: column;
   align-items: ${(props) => (props.isMaximised ? "flex-start" : "center")};
   position: fixed;
   left: 0;
   bottom: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
+  overflow-y: hidden;
   background-color: ${(props) =>
     props.isMaximised ? "rgba(255, 255, 255)" : "rgba(217, 217, 217, 0.4)"};
   transition: bottom 0.3s ease-in-out;
@@ -33,6 +36,14 @@ export const Header = styled(Box)`
   padding-left: 1rem;
   padding-right: 1rem;
   height: 4rem;
+  width: 100%;
 `;
 
-export default { ModalContainer, Header };
+export const PlayerContainer = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+export default { ModalContainer, Header, PlayerContainer };
