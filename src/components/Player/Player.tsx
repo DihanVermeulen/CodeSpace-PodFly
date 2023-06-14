3c294879639c1b6c0c8e99a094fbc9362adf21f3
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-14 16:39:38 +0200

    refactor: update JSX

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,5 +1,13 @@
-import { ModalContainer, Header, PlayerContainer } from "./Player.styled";
-import { IconButton, Box, Slider } from "@mui/material";
+import {
+  ModalContainer,
+  Header,
+  PlayerContainer,
+  LoadingStateContainer,
+  ContentContainer,
+  Image,
+  Title,
+} from "./Player.styled";
+import { IconButton, Slider, CircularProgress } from "@mui/material";
 import {
   Close,
   ArrowBack,
@@ -8,9 +16,10 @@ import {
 } from "@mui/icons-material";
 import { usePlayer } from "../../hooks/usePlayer";
 import { useAudioPlayer } from "../../hooks/useAudioPlayer";
+import { getModalState } from "../../model";
 
 export const Player = () => {
-  const { modalState, handleCloseModal, handleMaximiseModal } = usePlayer();
+  const { handleCloseModal, handleMaximiseModal } = usePlayer();
   const {
     audioRef,
     currentTime,
@@ -22,10 +31,10 @@ export const Player = () => {
     handlePlayPause,
   } = useAudioPlayer();
 
+  const modalState = getModalState();
   const isOpen = modalState.isOpen;
   const isMaximised = modalState.isMaximised;
   const playerData = modalState.data;
-
   return (
     <>
       <ModalContainer
@@ -36,9 +45,8 @@ export const Player = () => {
               }
             : () => {}
         }
-        isMaximised={isMaximised ? true : false}
+        isMaximised={isMaximised}
         isOpen={isOpen}
-        disableScrollLock
       >
         <Header>
           {modalState.isMaximised ? (
@@ -51,28 +59,57 @@ export const Player = () => {
             </IconButton>
           )}
         </Header>
-        <PlayerContainer>
-          {playerData && <audio ref={audioRef} src={playerData.audioSrc} />}
-          <Slider
-            value={currentTime}
-            min={0}
-            max={duration}
-            step={0.1}
-            onChange={handleProgressChange as any}
-            onMouseDown={handleDragStart}
-            onMouseUp={handleDragEnd}
-            onTouchStart={handleDragStart}
-            onTouchEnd={handleDragEnd}
-            sx={{ color: "#D7A6B3", width: "80%" }}
-          />
-          <IconButton onClick={handlePlayPause} size="large">
-            {isPlaying ? (
-              <PauseCircleFilled sx={{ fontSize: "4rem", color: "#AAF1CF" }} />
-            ) : (
-              <PlayCircleFilled sx={{ fontSize: "4rem", color: "#AAF1CF" }} />
-            )}
-          </IconButton>
-        </PlayerContainer>
+
+        {playerData && (
+          <>
+            <ContentContainer>
+              <Image src={playerData.image} />
+              <Title as="h1">{playerData.episodeTitle}</Title>
+              <p
+                style={{
+                  margin: 0,
+                  fontSize: 14,
+                  fontFamily: "Poppins",
+                  color: "#A1A1A1",
+                }}
+              >
+                Episode: {playerData.episodeNumber}
+              </p>
+            </ContentContainer>
+            <PlayerContainer>
+              <audio ref={audioRef} src={playerData.audioSrc} />
+              <Slider
+                value={currentTime}
+                min={0}
+                max={duration}
+                step={0.1}
+                onChange={handleProgressChange as any}
+                onMouseDown={handleDragStart}
+                onMouseUp={handleDragEnd}
+                onTouchStart={handleDragStart}
+                onTouchEnd={handleDragEnd}
+                sx={{ color: "#D7A6B3", width: "80%" }}
+              />
+              <IconButton onClick={handlePlayPause} size="large">
+                {isPlaying ? (
+                  <PauseCircleFilled
+                    sx={{ fontSize: "4rem", color: "#AAF1CF" }}
+                  />
+                ) : (
+                  <PlayCircleFilled
+                    sx={{ fontSize: "4rem", color: "#AAF1CF" }}
+                  />
+                )}
+              </IconButton>
+            </PlayerContainer>
+          </>
+        )}
+
+        {!playerData && (
+          <LoadingStateContainer sx={{ display: "flex" }}>
+            <CircularProgress variant="indeterminate" />
+          </LoadingStateContainer>
+        )}
       </ModalContainer>
     </>
   );
