704246c76a468de97d4ac49fcd7bc95ea4360d9a
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-15 17:01:21 +0200

    refactor: add update attr to DiscoverList

--- src/components/DiscoverList/DiscoverList.tsx
+++ src/components/DiscoverList/DiscoverList.tsx
@@ -29,6 +29,7 @@ export const DiscoverList = (props: DiscoverList) => {
                   title={item.title}
                   image={item.image}
                   seasons={item.seasons}
+                  updated={item.updated}
                 />
               </StyledLink>
             </Grid>
