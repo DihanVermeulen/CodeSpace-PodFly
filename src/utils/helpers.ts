89dcf8b47af56bc3f5923f0480deb50f2bc5a7cc
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-20 12:27:25 +0200

    refactor: add comments

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -199,7 +199,11 @@ export const removeEpisodeFromFavourites = (
     }
   });
 };
-
+/**
+ * Formats time in seconds and return string in the format mm:ss
+ * @param timeInSeconds
+ * @returns formatted time
+ */
 export const formatTime = (timeInSeconds: number) => {
   const minutes = Math.floor(timeInSeconds / 60);
   const seconds = Math.floor(timeInSeconds % 60);
