13b7f82f2f06c5da6e476d75d73237483f30b8c7
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-20 11:11:04 +0200

    refactor: add type safety

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -200,7 +200,7 @@ export const removeEpisodeFromFavourites = (
   });
 };
 
-export const formatTime = (timeInSeconds) => {
+export const formatTime = (timeInSeconds: number) => {
   const minutes = Math.floor(timeInSeconds / 60);
   const seconds = Math.floor(timeInSeconds % 60);
   const formattedMinutes = String(minutes).padStart(2, "0");
