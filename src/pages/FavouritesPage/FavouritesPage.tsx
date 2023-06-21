7ace7c6e907eef836be853071af7fa98416be3b0
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-21 14:30:37 +0200

    refactor: move into FavouritesList component

--- src/pages/FavouritesPage/FavouritesPage.tsx
+++ src/pages/FavouritesPage/FavouritesPage.tsx
@@ -1,39 +1,17 @@
 import { Filter } from "../../@types/filters";
 import { useAuth } from "../../hooks";
 import { Space } from "../../styles";
-import {
-  ToggleButton,
-  ToggleButtonGroup,
-  Box,
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Typography,
-  List,
-  ListItem,
-  ListItemText,
-  IconButton,
-} from "@mui/material";
+import { ToggleButton, ToggleButtonGroup, Box } from "@mui/material";
 import { useState, MouseEvent, useEffect } from "react";
-import {
-  BookmarkRemove,
-  ExpandMore,
-  PlayArrow,
-  Share,
-} from "@mui/icons-material";
-import { createFavouritesActions, getFavouritesState } from "../../model";
-import { removeEpisodeFromFavourites, shareContent } from "../../utils/helpers";
-import { format } from "date-fns";
-import { useNavigate } from "react-router-dom";
+import { getFavouritesState } from "../../model";
 import { IndividualPodcast } from "../../@types/podcast";
+import { FavouritesList } from "../../components/Lists/FavouritesList";
 
 export const FavouritesPage = () => {
   const { getSession } = useAuth();
   const session = getSession();
   const { favouritesList } = getFavouritesState();
   const [filter, setFilter] = useState<Filter>("ALL");
-  const favouritesActions = createFavouritesActions();
-  const navigate = useNavigate();
   const [filteredData, setFilteredData] = useState<IndividualPodcast[] | null>(
     null
   );
@@ -86,27 +64,6 @@ export const FavouritesPage = () => {
     }
   };
 
-  const handleRemoveEpisodeFromFavourites = (episodeID: string) => {
-    const userID = session?.user.id;
-    if (userID)
-      removeEpisodeFromFavourites({
-        episodeID,
-      }).then(async () => {
-        favouritesActions.getFavourites();
-        location.reload();
-      });
-  };
-
-  const handleNavigatetoPlayer = (
-    showID: string,
-    episodeNumber: number,
-    seasonNumber: number
-  ) => {
-    navigate(
-      `/listen?podcast=${showID}&season=${seasonNumber}&episode=${episodeNumber}`
-    );
-  };
-
   return (
     <>
       <Space height={"4rem"} />
@@ -145,107 +102,7 @@ export const FavouritesPage = () => {
       </Box>
       {session
         ? phase === "LOADED" &&
-          filteredData &&
-          filteredData.map((item) => (
-            <Accordion key={item.id}>
-              <AccordionSummary
-                expandIcon={<ExpandMore />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography>{item.title}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                {item.seasons.map((season) => (
-                  <Accordion key={`${item.id}${season.season}`}>
-                    <AccordionSummary
-                      expandIcon={<ExpandMore />}
-                      aria-controls="panel-content"
-                      id={`panel${item.id}season${season.season}-header`}
-                    >
-                      <Typography>Season: {season.season}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <List>
-                        {season.episodes.map((episode) => (
-                          <ListItem
-                            key={episode.id}
-                            secondaryAction={
-                              <>
-                                <IconButton
-                                  onClick={() =>
-                                    shareContent(
-                                      "PodFly",
-                                      "Go check out this awesome podcast on PodFly!",
-                                      `https://podfly-demo.netlify.app/listen?podcast=${item.id}&season=${season.season}&episode=${episode.episode}`
-                                    )
-                                  }
-                                >
-                                  <Share />
-                                </IconButton>
-                                <IconButton
-                                  onClick={() => {
-                                    if (episode.id) {
-                                      handleRemoveEpisodeFromFavourites(
-                                        episode.id
-                                      );
-                                    }
-                                  }}
-                                >
-                                  <BookmarkRemove />
-                                </IconButton>
-                                <IconButton
-                                  onClick={() =>
-                                    handleNavigatetoPlayer(
-                                      item.id,
-                                      episode.episode,
-                                      season.season
-                                    )
-                                  }
-                                >
-                                  <PlayArrow />
-                                </IconButton>
-                              </>
-                            }
-                          >
-                            <ListItemText
-                              primary={episode.title}
-                              secondary={
-                                <Box display="flex" flexDirection="column">
-                                  <Typography
-                                    sx={{ display: "inline" }}
-                                    component="span"
-                                    variant="body2"
-                                    color="#a1a1a1"
-                                  >
-                                    Episode {episode.episode}
-                                  </Typography>
-                                  <Typography
-                                    sx={{ display: "inline" }}
-                                    component="span"
-                                    variant="body2"
-                                    color="#a1a1a1"
-                                  >
-                                    added:{" "}
-                                    {episode.created_at &&
-                                      format(
-                                        new Date(episode.created_at),
-                                        "dd LLLL yyyy"
-                                      )}
-                                  </Typography>
-                                </Box>
-                              }
-                              disableTypography
-                            />
-                          </ListItem>
-                        ))}
-                      </List>
-                    </AccordionDetails>
-                  </Accordion>
-                ))}
-              </AccordionDetails>
-            </Accordion>
-          ))
+          filteredData && <FavouritesList data={filteredData} />
         : "please sign in"}
     </>
   );
